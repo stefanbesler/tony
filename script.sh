cc7ae3b4e3b70075b437e3c37b20e8c4f7eb1d67
Stefan Besler <stefan@besler.me>
2023-12-12 20:10:46 +0100

    fix: path

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "export TONY_USERNAME=\"$TONY_USERNAME\"" >> /env.sh
+echo "export TONY_USERNAME=\"$TONY_USERNAME\"" > /env.sh
 echo "export TONY_PASSWORD=\"$TONY_PASSWORD\"" >> /env.sh
 echo "export TONY_PLAYLIST=\"$TONY_PLAYLIST\"" >> /env.sh
 echo "export TONY_PUSHOVER_USERKEY=\"$TONY_PUSHOVER_USERKEY\"" >> /env.sh
